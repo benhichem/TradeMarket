7949e69aa4221b3b731deb35988940253195c0c2
hichemben <hichemben45@gmail.com>
2024-08-03 09:04:20 -0700

    created my setup

---
+++ features.md
@@ -0,0 +1,57 @@
+## User Story
+Scraper Brief – TrustATrader
+
+We require a scraper to be built that will use a list of search URLS to individually go into each business profile found and scrape specific information. When all the data has been scraped we then need to create a scraper to scrape the contact information (e-mail address) from the business website.
+
+Example search URL:
+
+https://www.trustatrader.com/search?trade_name=Locksmith&search_trade_id=5b4cbdcb8811d346b846da5f&location_str=Hastings&lat=&lon=&trader=&search_trader_id=
+
+Example business profile URL:
+
+https://www.trustatrader.com/traders/locks-and-glass-ltd-locksmith-dartford
+
+Data to scrape:
+
+• Company name
+• Tel
+• Mob
+• Website
+• Owner name
+• Company type i.e. Limited company
+
+
+## initial thoughts: 
+breaking down the scraper into two parts 
+
+1. first to scrape the tradesMarket website for every profile avaliable with website 
+2. then to find a way to scrape template the emails from the websites usually have a form contact us since its only a landing page. 
+
+
+## Steps 
+
+### step 1 
+- [ ] scrape TradesMarket 
+    - [ ] collect every profile avaliable 
+        the information needed 
+        * Company_name: string 
+        * Tel : string 
+        * Mob : string 
+        * Website : URL 
+        * Owner_name : string 
+        * Company_type :string 
+    - [ ] save input into a temporary json file 
+
+### step 2
+- [ ] read from temporary json file
+- [ ] Visit the website.
+- [ ] away to always locate the contact form.
+- [ ] scrape email if its publicly avaliable. 
+- [ ] Save output to csv.
+
+### cleanup Process 
+- [ ] delete the temporary json file after all objects finished. 
+
+
+
+
---
+++ src/wrappers/puppeteer_provider.ts
@@ -0,0 +1,99 @@
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+import { Page, Browser } from 'puppeteer'
+import { Logger } from '@destruct/logger'
+import { ScrapperInterface } from '../types/index.js'
+
+puppeteer.use(StealthPlugin())
+
+type LaunchOptions = Parameters<typeof puppeteer.launch>[0]
+
+export default class PuppeteerScrapper implements ScrapperInterface {
+  public $page: Page | null
+  private _browser: Browser | null
+  protected payload: Array<any>
+  public logger: Logger
+  private browser_option: LaunchOptions
+  private close: boolean
+
+  constructor(close: boolean, browser_options?: LaunchOptions) {
+    this.$page = null
+    this._browser = null
+    this.payload = []
+    this.logger = new Logger('puppeteer_provider')
+    this.browser_option = browser_options
+      ? browser_options
+      : {
+          headless: true,
+        }
+
+    this.close = close
+  }
+
+  public async navigate(url: string): Promise<void> {
+    if (this.$page !== null) {
+      await this.$page
+        .goto(url, { timeout: 0, waitUntil: 'networkidle2' })
+        .catch((err) => {
+          throw err
+        })
+    }
+  }
+  private async _setup() {
+    try {
+      this.logger.info('Initiating Browser ... ')
+      this._browser = await puppeteer.launch(this.browser_option)
+
+      this.$page = await this._browser.newPage()
+      if (this.$page) {
+        await this.$page.setViewport({
+          height: 1200,
+          width: 1500,
+        })
+      }
+      this.logger.info('Browser started successfully ...')
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error)
+        throw new Error('Puppeteer Failed To start')
+      }
+    }
+  }
+
+  public async _cleanup() {
+    this.logger.info('Clean up process started ...')
+    if (this.$page && this._browser) {
+      await this.$page.close()
+      this.$page = null
+      await this._browser.close()
+      this._browser = null
+    }
+  }
+
+  public async exists(selector: string): Promise<boolean> {
+    return await this.$page!.waitForSelector(selector, { timeout: 1000 })
+      .then(() => {
+        this.logger.info('Selector Exists ...')
+        return true
+      })
+      .catch((err) => {
+        this.logger.info('Selector does not exist ...')
+        return false
+      })
+  }
+  async restart() {
+    this.logger.info('Restarting The puppeteer session ...')
+    await this._cleanup()
+    await this._setup()
+  }
+  protected async $extract() {}
+
+  async exec() {
+    await this._setup()
+    await this.$extract()
+    this.close
+      ? await this._cleanup()
+      : this.logger.info('Browser needs to be manually closed ')
+    return this.payload
+  }
+}
